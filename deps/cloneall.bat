39c1f930b7a4d13385f6274712688ffef968545a
SergeyLebedkin <s.lebedkin@gmail.com>
2018-07-26 16:14:06 +0300

    Update cloneall.bat

--- deps/cloneall.bat
+++ deps/cloneall.bat
@@ -1,4 +1,4 @@
 git clone git@github.com:bulletphysics/bullet3.git
 git clone git@github.com:glfw/glfw.git
 git clone git@github.com:g-truc/glm.git
-git clone git@github.com:SergeyLebedkin/opengl.git
+git clone git@github.com:SergeyLebedkin/khronos.git
