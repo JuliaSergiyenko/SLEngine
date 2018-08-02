dbc38bc4d06cf9f9cab517e9f064f66c4641eff5
Sergii Liebodkin <s.lebedkin@gmail.com>
2018-08-02 22:42:47 +0300

    ImGui Shaders fixed

--- build/msvs2017/apps/render_gl4_app/imgui_impl_opengl4.cpp
+++ build/msvs2017/apps/render_gl4_app/imgui_impl_opengl4.cpp
@@ -31,7 +31,7 @@ static const GLchar* cVSShaderSource = R"(
 #version 430 core
 
 // attributes
-layout (location = 0) in vec3 aPosition;
+layout (location = 0) in vec2 aPosition;
 layout (location = 1) in vec4 aColor;
 layout (location = 4) in vec2 aTexCoord;
 
--- build/msvs2017/apps/render_gles3_app/imgui_impl_opengles3.cpp
+++ build/msvs2017/apps/render_gles3_app/imgui_impl_opengles3.cpp
@@ -31,7 +31,7 @@ static const GLchar* cVSShaderSource = R"(
 precision mediump float;
 
 // attributes
-layout (location = 0) in vec3 aPosition;
+layout (location = 0) in vec2 aPosition;
 layout (location = 1) in vec4 aColor;
 layout (location = 4) in vec2 aTexCoord;
 
@@ -50,7 +50,7 @@ void main()
 	vColor = aColor;
 
 	// find position
-	gl_Position = uProjMat * vec4(aPosition, 1.0);
+	gl_Position = uProjMat * vec4(aPosition, 0.0, 1.0);
 }
 
 )";
