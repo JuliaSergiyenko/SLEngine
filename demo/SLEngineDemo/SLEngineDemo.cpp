0b180e63ff46fc50c45f8deaf5f7d6df0cb91e48
Sergey Lebedkin <s.lebedkin@gmail.com>
2020-04-11 14:11:42 +0300

    tinyobj compilation fixes

--- demo/SLEngineDemo/SLEngineDemo.cpp
+++ demo/SLEngineDemo/SLEngineDemo.cpp
@@ -110,8 +110,9 @@ void SLEngineDemo::InitSponza()
 	std::vector<tinyobj::material_t> materials;
 	std::string warn;
 	std::string err;
-	tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, "data/A380/A380.obj", "data/A380/", true);
-	//tinyobj::LoadObj(&attribs, &shapes, &materials, &err, "data/sponza/sponza.obj", "data/sponza/", true);
+	//tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, "data/A380/A380.obj", "data/A380/", true);
+	tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, "data/sponza/sponza.obj", "data/sponza/", true);
+	std::cout << warn << std::endl;
 	std::cout << err << std::endl;
 
 	// allocate buffers
